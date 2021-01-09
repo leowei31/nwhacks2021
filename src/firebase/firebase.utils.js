6231030268119f152163c776a00327ecfe3a6726
Leo <leo.wei@alumni.ubc.ca>
2021-01-09 15:28:18 -0700

    added firebase API key and made app into a class instead of funcion

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const firebaseConfig ={
+    apiKey: "AIzaSyAfUQzf-Dh99mGT_8hSW5LFnBrVHDEl_vk",
+    authDomain: "menu-c0848.firebaseapp.com",
+    projectId: "menu-c0848",
+    storageBucket: "menu-c0848.appspot.com",
+    messagingSenderId: "497977718392",
+    appId: "1:497977718392:web:920fa3148daac0123d9f8d",
+    measurementId: "G-VHZCMK5FWC"
+  };
