592dbfe42c6f4de3aa23ca9e325bad02cb9eabe6
Jasmine Kwong <jasminekkwong@gmail.com>
2021-01-10 11:42:09 -0700

    menu overlay

--- src/components/menu/menu.component.jsx
+++ src/components/menu/menu.component.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-
-import MenuItem from '../menu-item/menu-item.component';
+import './menu.css';
 
 class Menu extends React.Component {
     constructor(props) {
@@ -8,22 +7,28 @@ class Menu extends React.Component {
     }
 
     render() {
-        if (!Array.isArray(this.props.menu)) {
+        if (!Array.isArray(this.props.menu) || this.props.menu.length === 0) {
             return (<div></div>)
         }
         return (
-            <div key={this.props.menu}>
+            <div className={"overlay"} key={this.props.menu}>
+                <div className={"menu-header"}>
+                    <h3 className={"menu-title"}> MENU </h3>
+                    <button type="button" className="close" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
                 {this.props.menu.map(category => (
-                    <table>
+                    <table className={"table"}>
                         <thead>
                         <tr>
-                            <th>{Object.keys(category)[0]}</th>
+                            <th colspan="2">{Object.keys(category)[0]}</th>
                         </tr>
                         </thead>
                         {category[Object.keys(category)[0]].map(menuItem => (
                             <tr>
-                                <th>{menuItem.name}</th>
-                                <th>{menuItem.price}</th>
+                                <td>{menuItem.name}</td>
+                                <td>{menuItem.price}</td>
                             </tr>
                         ))}
                     </table>
