28050953dd0cbc0e85bf3d095309038ffeef4893
garyhwu <garyh1998@gmail.com>
2021-01-09 16:54:40 -0800

    designed homepage

--- src/pages/home/homePage.page.jsx
+++ src/pages/home/homePage.page.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import './homePage.css';
 
-const HomePage = () => (
-    <div>
-        welcome to your menu
-    </div>
-)
+class HomePage extends React.Component {
+    render() {
+        return (
+            <div className="HomePage">
+                <h1 style={{color: "orange"}}>Welcome</h1>
+                <button>Place Order</button>
+                <button>Sign Up for Waitlist</button>
+            </div>
+            
+        )
+    }
+}
+// const HomePage = () => (
+//     <div>
+//         welcome to your menu
+//     </div>
+// )
 
 export default HomePage;
