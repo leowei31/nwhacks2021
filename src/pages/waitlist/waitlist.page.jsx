f5b9167b59b09b8bce7a8b0a8c895686530e6b6b
garyhwu <garyh1998@gmail.com>
2021-01-10 11:27:48 -0800

    waitlist updated

---
+++ src/pages/waitlist/waitlist.page.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import './waitlist.css';
+import {BASE_URL} from "../../constants";
+import axios from 'axios'
+import Menu from "../../components/menu/menu.component";
+
+class WaitlistPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {email: "", timestamp: null, partySize: 0, apiResponse: []};
+}
+
+
+  updateInput = event => {
+    const target = event.target
+    this.setState({[target.name]: target.value});
+  }
+
+  getMenu = event => {
+    event.preventDefault();
+    const url = BASE_URL.concat('/restaurant/').concat(this.state.rName).concat('/menu')
+    console.log(url)
+    axios.get(url).then(res => this.setState({apiResponse: res.data}))
+  }
+
+  render() {
+    console.log(this.state.apiResponse)
+    return (
+        <div className="placeOrder">
+            <form onSubmit={this.getMenu} className={"form-inline"}>
+                <div className={"form-group row"}>
+                    <label htmlFor="rName" className="sr-only">Restaurant Name</label>
+                <input
+                    type='text'
+                    className={"form-control"}
+                    placeholder="Restaurant Name"
+                    id={"rName"}
+                    name="rName"
+                    value={this.state.rName}
+                    onChange={this.updateInput} />
+                </div>
+                <input className={"btn btn-light"} type={"submit"} value={"Submit"}/>
+            </form>
+            <Menu menu={this.state.apiResponse}></Menu>
+
+            {/*<p>{this.state.apiResponse}</p>*/}
+        </div>
+
+    )
+  }
+
+}
+
+export default WaitlistPage;
